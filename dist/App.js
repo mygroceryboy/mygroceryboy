6a16a808f9e654b6a2797dcc07147ca4190af667
Manoj Chalode <chalodem@gmail.com>
2017-06-09 21:42:07 +0100

    added static file path. removed dist from gitignore as public folder is inside dist folder.

--- dist/App.js
+++ dist/App.js
@@ -1,31 +1,26 @@
-import * as path from 'path';
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as bodyParser from 'body-parser';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 // Creates and configures an ExpressJS web server.
 class App {
-
-    // ref to Express instance
-    public express: express.Application;
-
     //Run configuration methods on the Express instance.
     constructor() {
         this.express = express();
         this.middleware();
         this.routes();
     }
-
     // Configure Express middleware.
-    private middleware(): void {
+    middleware() {
         this.express.use(logger('dev'));
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
         this.express.use('/', express.static(path.join(__dirname, "./public")));
     }
-
     // Configure API endpoints.
-    private routes(): void {
+    routes() {
         /* This is just to get up and running, and to make sure what we've got is
          * working so far. This function will change when we start to add more
          * API endpoints */
@@ -38,7 +33,5 @@ class App {
         });
         this.express.use('/', router);
     }
-
 }
-
-export default new App().express;
+exports.default = new App().express;
