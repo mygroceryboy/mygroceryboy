455946b67d9eb1543e718c3f22ce941fd25cb07a
Manoj Chalode <chalodem@gmail.com>
2017-06-12 22:52:33 +0100

    added tab titles

--- bin/run.bat
+++ bin/run.bat
@@ -5,16 +5,20 @@ cd ~/code/github/mygroveryboy/
 tab=" --tab"
 options=()
 
+title[1]="mongo db"
 cmds[1]="mongod --dbpath data/"
 
+title[2]="nodemon watch"
 cmds[2]="nodemon dist/server.js"
 
+title[3]="tsc watch"
 cmds[3]="tsc --watch"
 
+title[4]="ng build watch"
 cmds[4]="cd dist/public && ng build --watch"
 
 for i in 1 2 3 4; do
-options+=($tab -e "bash -c '${cmds[i]} ; bash'" )
+options+=($tab -e "bash -c '${cmds[i]} ; bash'" -t "${title[i]}")
 done
 
 gnome-terminal "${options[@]}"
